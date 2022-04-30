142a1670ebf473e2bea31defd9e4608109f0e48b
System Administrator <root@fw1.kissel.ch>
2022-04-30 22:50:48 +0200

    update script and config-sample file

--- check-updates.sh
+++ check-updates.sh
@@ -2,13 +2,14 @@
 # Check if updates for Opnsense are available
 # Send an e-mail reminder if yes
 
-set -x
+# debug mode
+##set -x
+
+cd "$(dirname "$0")"
 
 # read config file config.cfg
 . config.cfg
 echo $from $to $smtp $auth $msg
-
-
 
 # Check Opnsense for updates
 updates=$(/usr/local/opnsense/scripts/firmware/check.sh)
@@ -16,22 +17,21 @@ updates=$(/usr/local/opnsense/scripts/firmware/check.sh)
 if [[ $updates != *$isupdate* ]]; then
   echo "Packages are up to date."
 else
-         echo 'Updates available'
+        echo 'Updates available'
 
         # Create email message:
         touch $msg
-        ##echo From: $from >> $msg
-        ##echo To: $to >> $msg
+        echo From: $from >> $msg
+        echo To: $to >> $msg
         echo Subject: Opnsense Updates ready to install! >> $msg
 	echo Content-Type: text/plain; charset="utf8" >> $msg
         echo "There are new updates ready to install for your Opnsense firewall." >> $msg
-        echo
+	echo "" >> $msg
+	# Copy script output to message
 	echo $updates >> $msg
+	# Send email message
         curl --ssl-reqd $smtp --mail-from $from --mail-rcpt $to --upload-file $msg -u $auth
+
+        # Delete message file
+	rm $msg
 fi
-
-
-
-
-
-#curl -v --ssl-reqd smtp://mail.cyon.ch --mail-from "admin@fw1.kissel.ch" --mail-rcpt mail@kissel.ch -u smtp@kissel.ch:YaYkt2HHkb6-q2 --upload-file email.txt
