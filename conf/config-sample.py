3dc5510872d5f7da91cfa8424dfeb74687238964
Michael Kissel <mail@kissel.ch>
2024-03-25 16:11:52 +0100

    init config sample

---
+++ conf/config-sample.py
@@ -0,0 +1,13 @@
+## config
+
+# Mail settings
+sender_email = "sender@address.ch"
+receiver_email = "receiver@address.ch"
+subject = "Opnsense updates available!"
+
+# Smtp settings
+smtp_user = "smtp@server.ch"
+smtp_password = "**********"
+
+# firmware check output
+isupdate = "Your packages are up to date."
